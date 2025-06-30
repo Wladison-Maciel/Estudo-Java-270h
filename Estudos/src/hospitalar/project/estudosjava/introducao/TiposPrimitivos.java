f5c8c4493e2797f4bf35d2183e795afbb7da4dd5
Wladison-Maciel <franciscowladison97@gmail.com>
2025-06-30 14:43:41 -0300

    Revisando tipos primitivos

---
+++ Estudos/src/hospitalar/project/estudosjava/introducao/TiposPrimitivos.java
@@ -0,0 +1,13 @@
+package hospitalar.project.estudosjava.introducao;
+
+public class TiposPrimitivos {
+    public static void main(String[] args) {
+        int age = 18;
+        float height = 1.69f;
+        double weight = 56.5;
+        char character = 'W';
+        boolean isBoolean = true;
+        byte smallNumber = 127;
+        long largeNumber = 8_000_000_000L;
+    }
+}
